a558ded4b99a218b82f75ba763eb52e2121bbf89
Willem Boender <willem.boender@etu.unilim.fr>
2024-04-29 09:41:01 +0200

    [Add] Language options

--- Nao_presentation/translations/translation_en_US.ts
+++ Nao_presentation/translations/translation_en_US.ts
@@ -8,6 +8,49 @@
             <comment>Text</comment>
             <translation type="vanished">Hello</translation>
         </message>
+        <message>
+            <source>Bienvenue</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bienvenue</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (1)</name>
+        <message>
+            <source>Bonjour je suis Nao.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Bonjour je suis Nao.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (2)</name>
+        <message>
+            <source>Nous avons 45 bassins de tailles différentes, 200 espèces et 3500 animaux. La capacité totale d’Odysseo est de 2 millions litres d’eau.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Nous avons 45 bassins de tailles différentes, 200 espèces et 3500 animaux. La capacité totale d’Odysseo est de 2 millions litres d’eau.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (3)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Il est toutefois nécessaire de respecter les consignes suivantes pour le bien-être des animaux et pour avoir une bonne visite :
+
+</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Il est toutefois nécessaire de respecter les consignes suivantes pour le bien-être des animaux et pour avoir une bonne visite :
+
+</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>et très important la roue des actions.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">et très important la roue des actions.</translation>
+        </message>
     </context>
     <context>
         <name>behavior_1/behavior.xar:/Timeline/behavior_layer1/keyframe1/Say</name>
@@ -15,6 +58,96 @@
             <source>Hello</source>
             <comment>Text</comment>
             <translation type="vanished">Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/après tunnel</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Veuillez aussi noter qu’après le tunnel, la visite n’est par terminée. Suivez les petits poissons sur le sol pour découvrir l’autre partie du monde marin. . Au final vous retrouverez notre vue subocéanique</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Veuillez aussi noter qu’après le tunnel, la visite n’est par terminée. Suivez les petits poissons sur le sol pour découvrir l’autre partie du monde marin. . Au final vous retrouverez notre vue subocéanique</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/drop/behavior_layer1/texte/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ne pas courir et ne pas faire de gestes brusques dans l’aquarium. Lorsque vous parlez, exprimez-vous à voix basse.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ne pas courir et ne pas faire de gestes brusques dans l’aquarium. Lorsque vous parlez, exprimez-vous à voix basse.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/entrée/behavior_layer1/texte/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Information très importante, le premier couloir de la visite consiste à la découverte.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Information très importante, le premier couloir de la visite consiste à la découverte.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/goodbye/behavior_layer1/texte/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Je vous souhaite une très bonne visite.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Je vous souhaite une très bonne visite.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/mots clés/behavior_layer1/phrase mot clé/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Notre moto est Connaitre, Aimer, Protéger. Nous ne pouvons pas aimer ce que nous ne connaissons pas et nous ne pouvons pas protéger ce que nous n’aimons pas.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Notre moto est Connaitre, Aimer, Protéger. Nous ne pouvons pas aimer ce que nous ne connaissons pas et nous ne pouvons pas protéger ce que nous n’aimons pas.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/photos R2/behavior_layer1/texte/python box</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source> Ne pas utiliser de flash pour les photos.</source>
+            <comment>Text</comment>
+            <translation type="unfinished"> Ne pas utiliser de flash pour les photos.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/reflexion/behavior_layer1/phrase/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Je vous recommande vraiment de retrouver notre Mascote Yoctopus, avant la fin du couloir et de faire le jeu qu'il propose. Je vais vous aider, il est juste avant les 3 aquariums à droite. Ils se ressemblent tous mais ne le sont pas. Yoctopus est une pieuvre très cool et intelligente, il vous en dira plus. J'espère que vous le trouverez.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Je vous recommande vraiment de retrouver notre Mascote Yoctopus, avant la fin du couloir et de faire le jeu qu'il propose. Je vais vous aider, il est juste avant les 3 aquariums à droite. Ils se ressemblent tous mais ne le sont pas. Yoctopus est une pieuvre très cool et intelligente, il vous en dira plus. J'espère que vous le trouverez.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/supermen position/behavior_layer1/phrase/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Bienvenue à Odysseo, ce magnifique lieu où vous allez découvrir le monde marin de l’ile Maurice et de l’Océan Indien.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Bienvenue à Odysseo, ce magnifique lieu où vous allez découvrir le monde marin de l’ile Maurice et de l’Océan Indien.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/supermen position2 (1)/behavior_layer1/phrase/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Merci et j'espère que vous en apprendrez davantage et que vous vous sentirez inspiré pour nous aider à protéger l'habitat sans lequel nous ne pouvons pas vivre : l'océan.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Merci et j'espère que vous en apprendrez davantage et que vous vous sentirez inspiré pour nous aider à protéger l'habitat sans lequel nous ne pouvons pas vivre : l'océan.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/supermen position2/behavior_layer1/phrase/Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Au cours de la visite, vous allez voir des panneaux informatifs qui vous informent sur la zone. N’hésitez pas à les lire pour avoir des informations et aussi pour savoir comment vous pouvez aider. Si vous voulez en apprendre plus sur les espèces, regardez les écrans. Vous verrez défiler les informations sur les espèces que vous pouvez observer.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Au cours de la visite, vous allez voir des panneaux informatifs qui vous informent sur la zone. N’hésitez pas à les lire pour avoir des informations et aussi pour savoir comment vous pouvez aider. Si vous voulez en apprendre plus sur les espèces, regardez les écrans. Vous verrez défiler les informations sur les espèces que vous pouvez observer.</translation>
         </message>
     </context>
 </TS>
