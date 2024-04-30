19a10bd54b3a60d61e468b189d7752337dab06cb
Willem Boender <willem.boender@etu.unilim.fr>
2024-04-30 12:12:19 +0200

    [improve] naoWebApp

--- WebAppNao/ServFlask.py
+++ WebAppNao/ServFlask.py
@@ -1,8 +1,7 @@
+# -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, redirect, url_for
 import socket
 from naoqi import ALProxy
-
-# coding: utf-8 
 
 app = Flask(__name__)
 
@@ -24,27 +23,26 @@ def index_page():
 @app.route('/move_forward', methods=['POST'])
 def move_forward():
     # Envoyer la commande au robot NAO pour avancer
-    motion_proxy.moveToward(0.5, 0, 0)
+    # motion_proxy.moveToward(0.5, 0, 0)
     return redirect(url_for('index_page'))
 
 @app.route('/move_backward', methods=['POST'])
 def move_backward():
     # Envoyer la commande au robot NAO pour reculer
-    motion_proxy.moveToward(-0.5, 0, 0)
+    # motion_proxy.moveToward(-0.5, 0, 0)
     return redirect(url_for('index_page'))
 
 # Ajoutez d'autres routes et fonctions pour d'autres mouvements du robot NAO
 
 if __name__ == '__main__':
     # Obtention de l'adresse IP de l'ordinateur
-    hostname = socket.gethostname()
-    adresse_ip = socket.gethostbyname(hostname)
-    print(f"Adresse IP : {adresse_ip}")
+    adresse_ip = socket.gethostbyname(socket.gethostname())
+    print("Adresse IP : %s" % adresse_ip)
 
     # Définition du port pour le serveur Flask
     port = 5000  # Par défaut, utilisez un autre port si nécessaire
 
-    print(f"Port : {port}")
+    print("Port : %d" % port)
 
     # Lancement du serveur Flask
-    app.run(host='0.0.0.0', port=port)
+    app.run(host=adresse_ip, port=port)
