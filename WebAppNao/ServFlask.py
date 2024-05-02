c394221b521c909ade4f0be5671059f9eb8d8b42
Willem Boender <willem.boender@etu.unilim.fr>
2024-05-02 08:25:57 +0200

    [update] WebAppNAO

--- WebAppNao/ServFlask.py
+++ WebAppNao/ServFlask.py
@@ -5,13 +5,18 @@ from naoqi import ALProxy
 
 app = Flask(__name__)
 
+
+
+
 # Adresse IP du robot NAO et port
-nao_ip = "192.168.8.100"  # À remplacer par l'adresse IP réelle de votre robot NAO
+nao_ip = "11.0.0.101"  # À remplacer par l'adresse IP réelle de votre robot NAO
 nao_port = 9559
+
+tts = ALProxy("ALTextToSpeech", "11.0.0.101", nao_port)
 
 # Connexion au robot NAO
 try:
-    motion_proxy = ALProxy("ALMotion", nao_ip, nao_port)
+    motion_proxy = ALProxy("ALMotion", "11.0.0.101", nao_port)
 except Exception as e:
     print("Erreur lors de la connexion au robot NAO:", e)
 
@@ -23,16 +28,22 @@ def index_page():
 @app.route('/move_forward', methods=['POST'])
 def move_forward():
     # Envoyer la commande au robot NAO pour avancer
-    # motion_proxy.moveToward(0.5, 0, 0)
+    motion_proxy.moveToward(0.5, 0, 0)
     return redirect(url_for('index_page'))
 
 @app.route('/move_backward', methods=['POST'])
 def move_backward():
     # Envoyer la commande au robot NAO pour reculer
-    # motion_proxy.moveToward(-0.5, 0, 0)
+    motion_proxy.say("Hello, world!")
     return redirect(url_for('index_page'))
 
 # Ajoutez d'autres routes et fonctions pour d'autres mouvements du robot NAO
+
+@app.route('/say_hello', methods=['POST'])
+def say_hello():
+    # Envoyer la commande au robot NAO pour reculer
+    tts.say("Hello, world!")
+    return redirect(url_for('index_page'))
 
 if __name__ == '__main__':
     # Obtention de l'adresse IP de l'ordinateur
