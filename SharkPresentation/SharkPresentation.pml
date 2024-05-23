37b00a19a9656a51197403532b028c8e8d6ebb8b
Willem Boender <willem.boender@etu.unilim.fr>
2024-05-23 10:30:12 +0200

    [add] NAO Shraks Script implémentation

---
+++ SharkPresentation/SharkPresentation.pml
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<Package name="SharkPresentation" format_version="5">
+    <Manifest src="manifest.xml" />
+    <BehaviorDescriptions>
+        <BehaviorDescription name="behavior" src="behavior_1" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="turnToTheCrowd" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="RaiseArm" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="FearSharks" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Thinking" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="InteruptOpenArms" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="TouchingEars" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Exclamation" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="WeAreSharks" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="No" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Excited" xar="behavior.xar" />
+        <BehaviorDescription name="behavior" src="Interst_about_sharks" xar="behavior.xar" />
+    </BehaviorDescriptions>
+    <Dialogs />
+    <Resources />
+    <Topics />
+    <IgnoredPaths />
+    <Translations auto-fill="en_US">
+        <Translation name="translation_en_US" src="translations/translation_en_US.ts" language="en_US" />
+    </Translations>
+</Package>
---
+++ SharkPresentation/translations/translation_en_US.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>Interst_about_sharks/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="Interst_about_sharks/behavior.xar" line="0"/>
+            <source>D'accord, peut-être que les requins sont plus intéressants que je ne le pensais. Mais y a-t-il des avantages à avoir des requins ?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">D'accord, peut-être que les requins sont plus intéressants que je ne le pensais. Mais y a-t-il des avantages à avoir des requins ?</translation>
+        </message>
+    </context>
+    <context>
+        <name>WeAreSharks/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="WeAreSharks/behavior.xar" line="0"/>
+            <source>Donc, il y a un petit peu de requin en chacun de nous !</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Donc, il y a un petit peu de requin en chacun de nous !</translation>
+        </message>
+    </context>
+</TS>
